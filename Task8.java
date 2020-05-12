11ad70599ee6c124160bc337cef1035629c5e324
Gifon <62992178+Gifon@users.noreply.github.com>
2020-05-13 01:51:04 +0300

    Update Task8.java

--- Task8.java
+++ Task8.java
@@ -19,40 +19,38 @@ public class Task8 {
             c1 /= 10;
         }
         int maxlen = Math.max(lenb, lenc); // наибольшее кол-во цифр из двух чисел
-        double bmas[] = new double[maxlen]; // вспомогательный массив чисел из первого числа
-        double cmas[] = new double[maxlen]; // вспомогательный массив чисел из второго числа
-        bmas[0] = b / (Math.pow(10, lenb - 1));
-        bmas[0] = (int) bmas[0];
-        cmas[0] = c / (Math.pow(10, lenc - 1));
-        cmas[0] = (int) cmas[0];
-        double bmasnew[] = new double[lenb]; // массив цифр, из которых состоит первое число
-        double cmasnew[] = new double[lenc]; // массив цифр, из которых состоит второе число
-        bmasnew[0] = bmas[0];
-        cmasnew[0] = cmas[0];
+        double bmas [] = new double[maxlen]; // вспомогательный массив чисел из первого числа
+        double cmas [] = new double[maxlen]; // вспомогательный массив чисел из второго числа
+        bmas[0] = b/(Math.pow(10,lenb-1));
+        bmas[0] = (long) bmas[0];
+        cmas[0] = c/(Math.pow(10,lenc-1));
+        cmas[0] = (long) cmas[0];
+        double bmasnew [] = new double[lenb]; // массив цифр, из которых состоит первое число
+        double cmasnew [] = new double[lenc]; // массив цифр, из которых состоит второе число
+        bmasnew [0] = bmas [0];
+        cmasnew [0] = cmas [0];
         for (int i = 1; i < maxlen; i++) {
-            bmas[i] = b / (Math.pow(10, lenb - 1 - i));
-            bmas[i] = (int) bmas[i];
-            bmasnew[i] = bmas[i] - bmas[i - 1] * 10;
-            if (i == lenb - 1) {
+            bmas[i] = b/(Math.pow(10,lenb-1-i));
+            bmas[i] = (long) bmas[i];
+            bmasnew[i] = bmas[i] - bmas[i-1]*10;
+            if (i == lenb-1) {
                 break;
             }
         }
         for (int j = 1; j < maxlen; j++) {
-            cmas[j] = c / (Math.pow(10, lenc - 1 - j));
-            cmas[j] = (int) cmas[j];
+            cmas[j] = c/(Math.pow(10,lenc-1-j));
+            cmas[j] = (long) cmas[j];
             cmasnew[j] = cmas[j] - cmas[j - 1] * 10;
-            if (j == lenc - 1) {
+            if (j == lenc-1) {
                 break;
             }
         }
-        for (int g = 0; g <= 9; g++) {
-            for (int f = 0; f < maxlen; f++) {
-                for (double el : bmasnew) {
-                    for (double ek : cmasnew) {
-                        if (el == g && ek == g) {
-                            System.out.println("В обоих числах содержится цифра " + g);
-                            g++;
-                        }
+        for (int g = 0; g <=9; g++) {
+            for (double el : bmasnew) {
+                for (double ek : cmasnew) {
+                    if (el == g && ek == g) {
+                        System.out.println("В обоих числах содержится цифра " + g);
+                        g++;
                     }
                 }
             }
